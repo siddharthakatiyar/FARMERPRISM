485de5187fdfe5b18aca50cf908a3cb02f838ce2
Ragini Mittal <117941081+Raginii06@users.noreply.github.com>
2023-01-11 15:53:47 +0530

    logo updated

--- src/Header.js
+++ src/Header.js
@@ -5,7 +5,7 @@ const Header = () => {
     <nav class="navbar bg-body-tertiary">
         <div class="container-fluid">
         <a class="navbar-brand" href="#">
-        <img src="/docs/5.3/assets/brand/bootstrap-logo.svg" alt="Logo" width="30" height="24" class="d-inline-block align-text-top" />
+        <img src="/Logo.jpeg" alt="Logo" width="30" height="24" class="d-inline-block align-text-top" />
             FARMERPRISM
         </a>
         </div>
