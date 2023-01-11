072f16ab8fa0c2c4c6dcc82beee34a91f19332c0
Ragini Mittal <raginimittal0603@gmail.com>
2023-01-11 16:07:39 +0530

    header change

--- src/Header.js
+++ src/Header.js
@@ -1,11 +1,12 @@
 import React from 'react';
 
 const Header = () => {
+ 
   return (
     <nav class="navbar bg-body-tertiary">
         <div class="container-fluid">
         <a class="navbar-brand" href="#">
-        <img src="/docs/5.3/assets/brand/bootstrap-logo.svg" alt="Logo" width="30" height="24" class="d-inline-block align-text-top" />
+        <img src="/Logo.jpeg" alt="" width="150" height="150" class="d-inline-block align-text-top" />
             FARMERPRISM
         </a>
         </div>
