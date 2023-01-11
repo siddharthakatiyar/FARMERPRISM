0f5f0633d63e3d26b4e67cf05fc6549ff975a1b3
Siddhartha Katiyar <siddharthakatiyar25@gmail.com>
2023-01-11 22:36:44 +0530

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/FARMERPRISM/precache-manifest.afa975a3103a0c9a07a44ef10102164d.js"
+  "/FARMERPRISM/precache-manifest.0c227622efc59ec2baf189a271f175c0.js"
 );
 
 workbox.clientsClaim();
