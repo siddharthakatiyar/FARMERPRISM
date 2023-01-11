508b9852377bb150d58926a7b80898f74ba43988
Siddhartha Katiyar <siddharthakatiyar25@gmail.com>
2023-01-11 17:15:25 +0530

    NA

--- src/Header.js
+++ src/Header.js
@@ -6,7 +6,7 @@ const Header = () => {
     <nav class="navbar bg-body-tertiary">
         <div class="container-fluid">
         <a class="navbar-brand" href="#">
-        <img src="%PUBLIC_URL%/logo.png" alt="Logo" width="30" height="24" class="d-inline-block align-text-top" />
+        <img src="https://github.com/siddharthakatiyar/FARMERPRISM/blob/3447b23c4bb3da07a693c737e2c14cf5b192be0f/public/logo.png" alt="Logo" width="30" height="24" class="d-inline-block align-text-top" />
             <h1>FARMERPRISM</h1>
         </a>
         </div>
