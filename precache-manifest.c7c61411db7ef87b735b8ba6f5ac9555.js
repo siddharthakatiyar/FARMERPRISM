649d49e7b22eb0ed8e08653d3f8df1a18a84e498
Ujjawal Goel <ugoel646@gmail.com>
2023-01-11 18:56:36 +0530

    Updates

---
+++ precache-manifest.c7c61411db7ef87b735b8ba6f5ac9555.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6267ab0271ecafb88418",
+    "url": "/FARMERPRISM/static/css/main.11d832ee.chunk.css"
+  },
+  {
+    "revision": "6267ab0271ecafb88418",
+    "url": "/FARMERPRISM/static/js/main.650a95b3.chunk.js"
+  },
+  {
+    "revision": "44283357878e613f016d",
+    "url": "/FARMERPRISM/static/js/runtime~main.d8f8aec8.js"
+  },
+  {
+    "revision": "fae13182d0d3478d006f",
+    "url": "/FARMERPRISM/static/js/2.40b568f5.chunk.js"
+  },
+  {
+    "revision": "01847d2f2cb6520e45abc969479f5d57",
+    "url": "/FARMERPRISM/index.html"
+  }
+];
