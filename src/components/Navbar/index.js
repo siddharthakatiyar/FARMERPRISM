2f11242eabb4533de882afad5affaa2cebcbde7b
Ujjawal Goel <ugoel646@gmail.com>
2023-01-13 00:14:43 +0530

    agriculture page developed

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -4,6 +4,7 @@ import { Nav, NavLink, NavMenu }
 
 const Navbar = () => {
 return (
+	<>
 	<Nav>
 		<NavMenu>
         <NavLink to="/index" activeStyle>
@@ -19,7 +20,9 @@ return (
 			Finance
 		</NavLink>
 		</NavMenu>
+		
 	</Nav>
+	</>
 );
 };
 
